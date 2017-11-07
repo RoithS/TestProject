08fe9a351c930ba2ddf72a522630a315828d2ed3
RoithS <6271671+RoithS@users.noreply.github.com>
2017-11-07 13:30:53 +0100

    Create TestMain.java

---
+++ src/testproject/TestMain.java
@@ -0,0 +1,12 @@
+package testproject;
+
+public class TestMain{
+
+  public static void main(String[] args){
+    System.out.println(TestMain.getGreetings());
+  } 
+  
+  private static String getGreeting() {
+    return "Hello Tester";
+  }
+}
